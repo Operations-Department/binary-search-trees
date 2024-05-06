b48a56166472812ad25e12d53d204f5b6517b8f4
Chris <129601042+Operations-Department@users.noreply.github.com>
2024-05-06 11:23:28 -0500

    add initial tree and node classes

---
+++ binary-search-tree.js
@@ -0,0 +1,15 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null;
+    }
+
+    
+}
