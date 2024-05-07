9de49243b16b078d5babb05676c6fafe94f739de
Chris <129601042+Operations-Department@users.noreply.github.com>
2024-05-06 19:11:53 -0500

    add functions to delete called node or insert new node

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -11,6 +11,7 @@ class BST {
         this.root = null;
     }
 
+    //create the tree from an array
     buildTree(array) {
         //sort array + filter duplicates
         array = [...new Set(array)].sort((a, b) => a - b);
@@ -34,8 +35,114 @@ class BST {
     
         return buildTreeHelper(0, array.length-1);
     }
+
+    //add new value/node to tree
+    insert(value) {
+        //create new node to be inserted
+        const newNode = new Node(value);
+
+        //if bst is empty insert as root
+        if (!this.root) this.root = newNode;
+    
+        //start at the root
+        let current = this.root;
+
+        //traverse the bst until you get to the right spot
+        while (true) {
+
+            //prevent duplicates
+            if (value === current.data) return;
+
+            //if value is left than current node - go left
+            if (value < current.data) {
+                if (!current.left) {
+                    current.left = newNode; //insert new node
+                    break;
+                } 
+                current = current.left;
+            } 
+            
+            //if value is greater than current node - go right 
+            else {
+                if (!current.right) {
+                    current.right = newNode; //insert new node
+                    break;
+                } 
+                current = current.right;
+            }
+        }
+    }
+
+    //remove value from tree
+    deleteItem(value) {
+
+        //tree doesn't exist
+        if (!this.root) return;
+        
+        //start at root
+        let current = this.root;
+        let prev = null;
+
+        //traverse down tree recursively
+        while (current) {
+            //found value
+            if (current.data === value) break;
+
+            prev = current;
+
+            //haven't found it yet
+            if (value < current.data && current.left) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+
+        if (!current) return;
+
+        //case 1: no children
+        if (!current.left && !current.right) {
+            if (prev) {
+                if (current === prev.left) {
+                    prev.left = null //skip over node
+                } else {
+                    prev.right = null; //skip over node
+                }
+            } else {
+                this.root = null; 
+            }
+        }
+
+        //case 2: one child exists
+        if (current.right && !current.left) prev.right = current.right; //skip over node
+        if (!current.right && current.left) prev.left = current.left; //skip over node
+        
+        //case 3: two children exist 
+        if (current.right && current.left) {
+            //set variables to go get the lowest value from the right subtree
+            let fetcher = current.right;
+            let fetcherParent = null;
+            
+            //right child doesn't have a left child
+            if (!fetcher.left) { 
+                current.right = fetcher.right;
+            }  
+
+            //recursively go down the left line until the end
+            while (fetcher.left) {
+                fetcherParent = fetcher;
+                fetcher = fetcher.left;
+            };
+            
+            //replace the current node with the fetched node
+            current.data = fetcher.data;
+            //remove the fetched node from tree
+            if (fetcherParent) fetcherParent.left = null;
+        }
+    }
 }
 
+//print visualization in console
 function prettyPrint(node, prefix = "", isLeft = true) {
     if (node === null) return;
     if (node.right !== null) prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
@@ -43,11 +150,30 @@ function prettyPrint(node, prefix = "", isLeft = true) {
     if (node.left !== null) prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
 };
 
-let myTree = new BST();
-
 // let arr = [1, 2, 3, 4, 5, 6, 7];
 let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
+let myTree = new BST();
+
 myTree.root = myTree.buildTree(arr, 0, arr.length-1);
+
+myTree.insert(55);
+myTree.insert(101);
+myTree.insert(-1);
+myTree.insert(9);
+myTree.insert(155);
+myTree.insert(1101);
+myTree.insert(-11);
+myTree.insert(19);
+myTree.insert(525);
+myTree.insert(1201);
+myTree.insert(-21);
+myTree.insert(92);
+myTree.insert(1255);
+myTree.insert(12101);
+myTree.insert(-211);
+myTree.insert(129);
+
+myTree.deleteItem(67);
 
 prettyPrint(myTree.root);
